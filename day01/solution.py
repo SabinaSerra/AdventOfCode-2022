ac9cb0d916801f644c3630740d9f65257f06da30
SabinaSerra <Sabina.serra@cygni.se>
2022-12-01 06:56:38 +0100

    Day01

---
+++ day01/solution.py
@@ -0,0 +1,35 @@
+
+from os import environ
+
+def read_input(file_name):
+    input_vals = []
+    with open(file_name) as f:
+        curr_vals = []
+        for row in f:
+            if row == '\n':
+                input_vals.append(curr_vals)
+                curr_vals = []
+            else:
+                curr_vals.append(int(row.strip()))
+    return [sum(cals) for cals in input_vals]
+
+def solve_part2(input_vals):
+    num_vals = 3
+    res = []
+    for _ in range(num_vals):
+        val = max(input_vals)
+        res.append(val)
+        input_vals.remove(val)
+    return sum(res)
+
+def main():
+    part = environ.get('part')
+    input_vals = read_input("input.txt")
+    if part == 'part1':
+        print(max(input_vals))
+    else:
+        print(solve_part2(input_vals))
+
+
+if __name__ == "__main__":
+    main()
