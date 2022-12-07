d848cd660de35407f4882d83dd2353171b99102f
SabinaSerra <Sabina.serra@cygni.se>
2022-12-07 20:19:30 +0100

    Day07: Clean up

--- day07/solution.py
+++ day07/solution.py
@@ -9,62 +9,51 @@ HOME = "/"
 UP = ".."
 
 def solve_part2(input_data):
-    dirs = get_dir_sizes(input_data)
     needed_size = 30000000
     tot_size = 70000000
-    size_to_free = needed_size - (tot_size - dirs["/"]) 
+    size_to_free = needed_size - (tot_size - input_data[""]) 
     smallest_size = tot_size
-    smallest_dir = ""
-    for dir, size in dirs.items():
+    for size in input_data.values():
         if size_to_free < size < smallest_size:
-            smallest_dir = dir
             smallest_size = size
-    return smallest_dir, smallest_size
+    return smallest_size
 
-def get_dir_sizes(input_data):
-    dirs = defaultdict(lambda: 0)
-    for file, size in input_data.items():
-        parts = file.split("/")
-        part = ""
-        for i in range(len(parts)-1):
-            part += parts[i] + "/"
-            dirs[part] += int(size)
-    return dirs
-    
+ 
 def solve_part1(input_data):
-    dirs = get_dir_sizes(input_data)
-    res = []
     res_size = 0
-    for dir, size in dirs.items():
+    for size in input_data.values():
         if size <= 100000:
             res_size += size
-            res.append(dir)
     return res_size
+
+def move(curr_dir, move_to):
+    if move_to == HOME:
+        return ""
+    elif move_to == UP:
+        return curr_dir.rsplit("/", 1)[0]
+    else: 
+        return f"{curr_dir}/{move_to}"
+
+
+def add_file_size(path, file_size, dir_sizes):
+    dir_sizes[f"{path}"] += file_size
+    if path:
+        add_file_size(path.rsplit("/", 1)[0], file_size, dir_sizes)
 
 
 def parse_input(filename):
     with open(filename) as f:
         input_data = f.read().split("\n")
     curr_dir = ""
-    data = {}
-    for line_nr in range(len(input_data)):
-        line = input_data[line_nr]
-        values = line.split(" ")
-        if values[0] == COMMAND:
-            if values[1] == MOVE:
-                if values[2] == HOME:
-                    curr_dir == "/"
-                elif values[2] == UP:
-                    curr_dir = "/".join(curr_dir.split("/")[:-1])
-                else:
-                    curr_dir +=  f"/{values[2]}"
-            elif values[1] == SHOW:
-                pass
-        elif values[0] == DIR:
-            pass
-        else:
-            data[f"{curr_dir}/{values[1]}"] = values[0]
-    return data
+    dir_sizes = defaultdict(lambda: 0)
+    for line in input_data:
+        args = line.split(" ")
+        if args[0] == COMMAND and args[1] == MOVE:
+            curr_dir = move(curr_dir, args[2])
+        elif args[0] not in [COMMAND, DIR]:
+            add_file_size(curr_dir, int(args[0]), dir_sizes)
+    return dir_sizes
+
 
 def main():
     part = environ.get('part')
